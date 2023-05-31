8be011d74498e76ff91b475ea6c12144795251d2
Gawtham <gawtham.suprfam@gmail.com>
2023-05-31 16:04:48 +0530

    listings and reservation

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,9 +1,7 @@
-"use client"
-
 import { SafeUser } from "@/app/types"
-import React from "react"
-import Container from "../Container"
+
 import Categories from "./Categories"
+import Container from "../Container"
 import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from "./UserMenu"
@@ -15,9 +13,23 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed z-10 w-full bg-white shadow-sm">
-      <div className="border-b-[1px py-4">
+      <div
+        className="
+          border-b-[1px] 
+          py-4
+        "
+      >
         <Container>
-          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+          <div
+            className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+          >
             <Logo />
             <Search />
             <UserMenu currentUser={currentUser} />
